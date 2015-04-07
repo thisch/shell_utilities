55c19b81ef7ea186a978d8512b8dcee973f4f55d
jdoppler <joerg.doppler@tuwien.ac.at>
2015-04-07 09:40:27 +0200

    replace VSC -> VSC3

--- vsc_backup.sh
+++ vsc_backup.sh
@@ -3,10 +3,10 @@
 # (add to crontab via 'crontab -e')
 
 SRC="~"
-DEST="$HOME/VSC_backup"
+DEST="$HOME/VSC3_backup"
 OLD="$DEST/deleted_files"
 
 DATE=$(date "+%Y_%m_%d-%H:%M:%S")
 LOGFILE="$DEST/LOGFILES/rsync_log_$DATE.log"
 
-rsync --delete -abvzPe ssh vsc:$SRC $DEST --backup-dir=$OLD > $LOGFILE
+rsync --delete -abvzPe ssh vsc3:$SRC $DEST --backup-dir=$OLD > $LOGFILE
