ee934a5fd5ecc3c053b5b77687655f8187e3e083
jdoppler <joerg.doppler@tuwien.ac.at>
2015-04-11 07:58:24 +0200

    increase .png quality

--- convert.sh
+++ convert.sh
@@ -4,5 +4,5 @@
 
 for i in "$@"; do
     BASE=$(basename $i .jpg)
-    convert ${BASE}.jpg -fuzz 15% -transparent white -trim -resize 700x70! ${BASE}_resize.png
+    convert ${BASE}.jpg -fuzz 15% -transparent white -trim -resize 700x70! -quality 99 ${BASE}_resize.png
 done
