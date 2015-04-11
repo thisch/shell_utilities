53140045a970e92b656eaf75ed66f343c03d3057
jdoppler <joerg.doppler@tuwien.ac.at>
2015-04-11 14:25:10 +0200

    added -density to convert call

--- convert.sh
+++ convert.sh
@@ -4,6 +4,8 @@
 
 for i in "$@"; do
     BASE=$(basename $i .jpg)
-    convert ${BASE}.jpg -fuzz 15% -transparent white -trim -resize 700x70! -quality 99 ${BASE}_resize.png
-    convert ${BASE}_resize.png -threshold 99% -quality 99 ${BASE}_black.png
+    convert ${BASE}.jpg -fuzz 15% -transparent white -trim -resize 700x70! -quality 99 -density 600 ${BASE}_resize.png
+    convert ${BASE}_resize.png -threshold 99% -quality 99 -density 600 ${BASE}_black.png
+    # convert ${BASE}.jpg -fuzz 15% -transparent white -trim -resize 700x70! -define png:compression-level=0 -define png:compression-filter=0 -define png:color-type=2 ${BASE}_resize.png
+    # convert ${BASE}_resize.png -threshold 99% -define png:compression-level=0 -define png:compression-filter=0 -define png:color-type=2 ${BASE}_black.png
 done
